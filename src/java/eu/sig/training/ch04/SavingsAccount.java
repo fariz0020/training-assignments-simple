432182b563dbf333e1106b98c8dbf9365e92e07a
fariznurzam <t90fariz@gmail.com>
2019-04-14 20:47:53 +0700

    refactor savingaccount:20

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -9,15 +9,11 @@ public class SavingsAccount {
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
         // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
+        int sum = iterateCounterAccount(counterAccount);
         if (sum % 11 == 0) {
             // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+            CheckingAccount acct =
+                    Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount); // <2>
             // 3. Check whether withdrawal is to registered counter account:
             if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
@@ -29,6 +25,16 @@ public class SavingsAccount {
         } else {
             throw new BusinessException("Invalid account number!!");
         }
+    }
+
+    private int iterateCounterAccount(String counterAccount) {
+        int sum = 0;
+        for (int i = 0; i < counterAccount.length(); i++) {
+            char character = counterAccount.charAt(i);
+            int characterValue = Character.getNumericValue(character);
+            sum = sum + (9 - i) * characterValue;
+        }
+        return sum;
     }
 
     public void addInterest() {
