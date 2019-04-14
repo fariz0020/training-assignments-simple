3916b29892a136bda6071740c054a33cc56f5b07
fariznurzam <t90fariz@gmail.com>
2019-04-14 20:52:35 +0700

    refactor savingaccount

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -6,18 +6,12 @@ public class SavingsAccount {
     private Money balance = new Money();
     private CheckingAccount registeredCounterAccount;
 
-    public Transfer makeTransfer(String counterAccount, Money amount) 
-        throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
+    public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
         int sum = iterateCounterAccount(counterAccount);
         if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct =
-                    Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
+            CheckingAccount acct =  Accounts.findAcctByNumber(counterAccount);
+            Transfer result = new Transfer(this, acct, amount);
+            if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
                 return result;
             } else {
                 throw new BusinessException("Counter-account not registered!");
